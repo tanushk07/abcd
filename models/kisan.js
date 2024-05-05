a2469f333500db513f1ea8a87c012e2734f70977
tanushk <tanushk2004@gmail.com>
2024-05-05 23:19:44 +0530

    abcd

---
+++ models/kisan.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  pswd: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  Email: {
+    type: String,
+    required: true,
+  },
+  country: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  cart:[{
+    id:{
+      type:Schema.Types.ObjectId,
+      ref:'products'
+    },
+    quantity:Number
+  }]
+});
+
+module.exports = mongoose.model("users", UserSchema); //creating the model of users with schema
